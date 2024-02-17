4ab31485fbfd9b6434bcd8e7bcc27a5272754de2
xMikux <26039249+xMikux@users.noreply.github.com>
2024-02-17 18:16:44 +0800

    控制台翻譯

--- server/console.php
+++ server/console.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'console' => 'Console',
-    'node-under-maintenance' => 'The node of this server is currently under maintenance and all actions are unavailable.',
-    'running-installation-process' => 'This server is currently running its installation process and most actions are unavailable.',
-    'being-transferred' => 'This server is currently being transferred to another node and all actions are unavailable.',
-    'type-a-command' => 'Type a command...',
+    'console' => '控制臺',
+    'node-under-maintenance' => '此節點目前正在進行維護，所有的動作將不可用。',
+    'running-installation-process' => '此伺服器正在執行安裝程序，幾乎所有的動作將不可用。',
+    'being-transferred' => '此伺服器正在轉移至其他節點，所有動作將不可用。',
+    'type-a-command' => '輸入指令……',
 ];
